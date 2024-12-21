f719f6c82c9d084e107a0bc14aa8f3a542c08641
rapidausse <anthony.prunck06@orange.fr>
2024-12-21 23:18:16 +0100

    add function to switch on/off all the led

--- interface_space_ship.ino
+++ interface_space_ship.ino
@@ -1,10 +1,28 @@
 int switchState = 0;
 
+
+void allLedOn() {
+      digitalWrite(3, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+}
+
+void allLedOFf() {
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+}
+
 void setup() {
   pinMode(2, INPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
+
+  delay(250);
+  allLedOn();
+   delay(500);
+   allLedOFf();
 
 }
 
@@ -12,9 +30,7 @@ void loop() {
   switchState= digitalRead(2);
 
   if (switchState == LOW) {
-      digitalWrite(3, HIGH);
-      digitalWrite(4, LOW);
-      digitalWrite(5, LOW);
+      allLedOFf();
 
   } else {
       digitalWrite(3, LOW);
@@ -25,6 +41,16 @@ void loop() {
       digitalWrite(4, HIGH);
       digitalWrite(5, LOW);
       delay(250);
-
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      delay(250);
+      allLedOn();
+      delay(500);
+      allLedOFf();
+      delay(500);
+      allLedOn();
+      delay(500);
   }
 }
+
